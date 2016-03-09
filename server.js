04d35c39599e434f8a4c7df9a0de84f9cb94cefb
Charlie Chong <Charlie Chong>
2016-03-08 19:24:33 -0700

    replaced previous version

--- server.js
+++ server.js
@@ -1,34 +1,51 @@
 var http = require("http");
 var fs = require("fs");
-var path = require("path");
-var mime = require("mime");
-var port_number = process.env.PORT || 3000;
 
-function onRequest(request, response) {
-
-    if( request.method == 'GET' && request.url == '/' ){
-        response.writeHead(200, {"Content-Type": "text/html"});
-        //Open file as readable stream, pipe stream to response object
-        fs.createReadStream("./thisisourwebsite.html").pipe(response);
-    }else{
-        send404Response(response);
-    }
-
-}
+//We will send them a 404 response if page doesn't exist
 function send404Response(response){
     response.writeHead(404, {"Content-Type": "text/plain"});
     response.write("Error 404 - Page not found");
     response.end();
 }
 
-http.get('pg-api.sensorup.com/st-playground/proxy/v1.0/Datastreams(249162)/Observations', {
-    headers: {'St-P-Access-Token': '78cc7eb2-9394-4675-b231-ff0a377a3674'}
-});
+//Handle their request
+function onRequest(request, response) {
+	console.log(request.url);
+	console.log(request.url.indexOf('.html'));
+
+    if( request.method == 'GET' && request.url == '/' ){
+        response.writeHead(200, {"Content-Type": "text/html"});
+        //Open file as readable stream, pipe stream to response object
+        fs.createReadStream("./index.html").pipe(response);
+    }
+
+    else if( request.url== '/css/layouts/marketing.css' ){ //req.url has the pathname, check if it conatins '.css'
+	console.log('yay');
+      fs.readFile('./css/layouts/marketing.css', function (err, data) {
+        if (err) console.log(err);
+        response.writeHead(200, {'Content-Type': 'text/css'});
+        response.write(data);
+        response.end();
+      });
+    }
+
+     else if( request.url== '/img/common/file-icons.png' ){ //req.url has the pathname, check if it conatins '.png'
+      fs.readFile('./img/common/file-icons.png', function (err, data) {
+        if (err) console.log(err);
+        response.writeHead(200, {'Content-Type': 'image/png'});
+        response.write(data);
+        response.end();
+      });
+    }
+
+    else{
+        send404Response(response);
+    }
+
+}
 
 
-http.createServer(onRequest).listen(port_number);
+http.createServer(onRequest).listen(8888);
 console.log("Server is now running...");
 
-console.log(http.get('pg-api.sensorup.com/st-playground/proxy/v1.0/Datastreams(249162)/Observations', {
-    headers: {'St-P-Access-Token': '78cc7eb2-9394-4675-b231-ff0a377a3674'}
-}))
+//For 404 - http://localhost:8888/cornbacon
