9d8e763ccb0dc65dfea3de5a2eefb77add89ac7d
Charlie Chong <Charlie Chong>
2016-03-15 14:41:16 -0600

    comments

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ function onRequest(request, response) {
 }
 
 
-http.createServer(onRequest).listen(/*process.env.PORT || 3000*/8888);
+http.createServer(onRequest).listen(process.env.PORT || 3000);
 console.log("Server is now running...");
 
 
